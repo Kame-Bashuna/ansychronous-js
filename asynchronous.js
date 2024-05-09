169c2c8218a8d4a92ebffccd85769b6393ea46b4
Kame-Bashuna <basshunakameroba@gmail.com>
2024-05-09 06:54:47 +0300

    add change

---
+++ asynchronous.js
@@ -0,0 +1,26 @@
+function expectedMessage(message,delayTime){
+    setTimeout(()=>{
+        console.log(message) },delayTime)
+}
+expectedMessage("I am home",1000)
+
+const userIds=[user1,user2]
+async function userData(){
+    for (const userId of userId){
+        try{
+            const userInformation=await
+            userData(userIds)
+            console.log(userInformation)
+        }
+        catch(error){
+           console.log(error)
+        }
+}
+}
+function userInformation(id){
+    return Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const userInfo={}
+        })
+    })
+}
