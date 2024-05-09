d470d06341e91d15fb1dcb69f95f5cc92570c141
Kame-Bashuna <basshunakameroba@gmail.com>
2024-05-09 10:33:55 +0300

    add file

--- asynchronous.js
+++ asynchronous.js
@@ -4,29 +4,58 @@ function expectedMessage(message,delayTime){
 }
 expectedMessage("I am home",1000)
 
-const userIds=[user1,user2]
-async function userData(){
-    for (const userId of userId){
-        try{
-            const userInformation=await
-            userData(userIds)
-            console.log(userInformation)
-        }
-        catch(error){
-           console.log(error)
-        }
-}
-}
-function userInformation(id){
-    return Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            const userInfo={id,name:`user$id`,age:30}
-            resolve(userIds)
 
-        },3000)
+//no3
+async function callPerformTask(){
+    try{
+        await callPerformTask()
+        console.log(`task achieved`)
+    }
+    catch (error){
+        console.log(`error`, + error)
+    }
+}
+function performTask(){
+    return newPromise((resolve,reject)=>{
+        const success =true
+        setTimeout(()=>{
+        if(success){
+          resolve()
+        }
+else{
+    reject(`it is not right`)
+}},1000);
     })
 }
-userData()
+callPerformTask();
 
 
 
+//no2
+const userIds=['user1','user2','user3'];
+async function fetchAndLogUserData(){
+    for(const userId of userIds){
+        try{
+            const userId = await
+            getUserData(userId)
+            console.log(userData)
+        }
+        catch(error){
+            console.log(` error has happened for user ${userId}:${error}`);
+        }
+    }
+}
+function getUserData(id){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            const userData = {id,name:`user ${id},age:20`}
+            resolve(userData)
+        },1000)
+    });
+};
+fetchAndLogUserData()
+
+
+
+
+ 
